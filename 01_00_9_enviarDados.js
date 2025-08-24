7a463405dd0d06faadbe10c0c4016300d551120a
GiovanniRosadosAnjos <giovanni.anjos2014@gmail.com>
2025-08-24 14:13:26 -0300

    Add files via upload

---
+++ 01_00_9_enviarDados.js
@@ -0,0 +1,47 @@
+const endpoint = 'https://sheetdb.io/api/v1/ygjx7hr6r521t';
+
+async function buscarEmpresas( ) {
+  const res = await fetch(endpoint);
+  if (!res.ok) throw new Error('Erro ao buscar empresas');
+  return await res.json();
+}
+
+async function buscarRevisoesPorId(id) {
+  const res = await fetch(`${endpoint}/search?Id=${id}`);
+  if (!res.ok) throw new Error('Erro ao buscar revisões');
+  return await res.json();
+}
+
+async function salvarEmpresa(dados) {
+  const res = await fetch(endpoint, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ data: dados })
+  });
+  if (!res.ok) throw new Error('Erro ao salvar empresa');
+  return await res.json();
+}
+
+async function salvarContato(dados) {
+  const res = await fetch(`${endpoint}?sheet=Contato`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ data: dados })
+  });
+  if (!res.ok) throw new Error('Erro ao salvar contato');
+  return await res.json();
+}
+
+async function buscarContatosPorId(id) {
+  const res = await fetch(`${endpoint}/search?Id=${id}&sheet=Contato`);
+  if (!res.ok) throw new Error('Erro ao buscar contatos');
+  return await res.json();
+}
+
+export { 
+  buscarEmpresas, 
+  buscarRevisoesPorId, 
+  salvarEmpresa, 
+  salvarContato, 
+  buscarContatosPorId 
+};
